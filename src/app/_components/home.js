efac857cb909638fb91c776532cabc5896c727da
sha <abc@gmail.com>
2024-07-23 01:29:49 +0530

    added media queries on cards

--- src/app/_components/home.js
+++ src/app/_components/home.js
@@ -3,6 +3,7 @@
 import Link from 'next/link';
 import '../../../public/style.css'
 import Category from './category';
+import Image from 'next/image';
 
 export default function Home({ data }) {
   const dataElements = [];
@@ -10,20 +11,20 @@ export default function Home({ data }) {
         dataElements.push(
           <div key={data[i].id} className="container card-container mt-5" >
             <div className="card" >
-              <div className="row g-0 w-100">
+              <div className="row g-0 w-100 iic">
                 <div className="col-md-4 image-container" 
                 style={{ height: data[i].category === 'Entertainment' ? '200px' : 'auto' }}>
-                  <img src={data[i].img_url || 'https://static.vecteezy.com/system/resources/previews/000/197/882/original/vector-news-headlines-background-with-earth-planet.jpg'} className="img-fluid" alt="Card image"  />
+                  <Image className="c-img" height={200} width={500} src={data[i].img_url || 'https://static.vecteezy.com/system/resources/previews/000/197/882/original/vector-news-headlines-background-with-earth-planet.jpg'} className="img-fluid" alt="Card image" />
                 </div>
                 <div className="col-md-8">
                   <div className="card-body">
-                    <span className="card-title" style={{ color: '#44444D', fontSize:"22px",lineHeight:"27px",fontWeight:"350" }}>{data[i].title}</span>
+                    <p className="card-title" style={{ color: '#44444D', fontSize:"22px",lineHeight:"27px",fontWeight:"350" }}>{data[i].title}</p>
                     <p className="card-text">
-                      <span style={{fontStyle:"italic", fontSize:"14px", fontWeight:"300"}}>Source: {data[i].source} {data[i].date}</span>
+                      <span className="card-text" style={{fontStyle:"italic", fontSize:"14px", fontWeight:"300"}}>Source: {data[i].source} {data[i].date}</span>
                       <br />
-                      <span style={{color: '#44444D',fontSize:"16px",lineHeight:"22px",marginTop:"8px",fontWeight:"300"}}>{data[i].content}</span>
+                      <span className="card-text" style={{color: '#44444D',fontSize:"16px",lineHeight:"22px",marginTop:"8px",fontWeight:"300"}}>{data[i].content}</span>
                       <br />
-                      <Link href={data[i].read_more} target="_blank" rel="noopener noreferrer" className="read-more">Read More</Link>
+                      <Link className="card-text read-more" href={data[i].read_more} target="_blank" rel="noopener noreferrer">Read More</Link>
                     </p>
                   </div>
                 </div>
