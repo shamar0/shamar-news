e146d3ded8073ced285e95d93743f3640c40c6bf
sha <abc@gmail.com>
2024-08-05 00:45:31 +0530

    added about news app

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
           <h1>Hey! I Am Mohammad Shamar</h1>
           <p>Ambitious Software Developer with excellent frontend and backend development skills.</p>
           <div className="hero-buttons">
-            <Link href="https://orange-dorice-39.tiiny.site/">
+            <Link href="https://drive.google.com/file/d/14U3ywfhnmcNWI-akd9J_KKT8WALrSQu8/view">
               Resume
             </Link>
           </div>
@@ -34,6 +34,9 @@ export default function Home() {
           <li><strong>Live News Updates:</strong> The website gathers real-time news from several reputable sources, ensuring that users always have access to the latest information.</li>
           <li><strong>Smooth Navigation:</strong> The site is designed to handle large amounts of news efficiently, allowing users to browse through articles easily with features like pagination.</li>
           <li><strong>Modern and Responsive Design:</strong> Built with the latest web technologies, the website offers a sleek and modern look that works well on both desktop and mobile devices.</li>
+          <li><strong>User Login and Signup:</strong> Provides user authentication features, allowing users to create accounts, log in, and access personalized features.</li>
+          <li><strong>Article Saving:</strong> Logged-in users can save specific articles for future reference, enhancing the user experience and convenience.</li>
+          <li><strong>Share Button:</strong>  Each news article includes a share button, enabling users to easily share content across social media platforms.</li>
           <li><strong>Voice Assistant Integration(using chrome extension):</strong>Made a chrome extension by using it users can navigate the website using voice commands such as "next page or go to next page", "previous page", "refresh", "scroll down", and "scroll up", making it more accessible and interactive.</li>          
         </ul>
 
