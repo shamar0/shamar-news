2136456de24efede5a46edf5b51291b46e9f6eea
sha <abc@gmail.com>
2024-09-08 01:00:08 +0530

    structured code

--- src/app/category/[catgorized_news]/page.js
+++ src/app/category/[catgorized_news]/page.js
@@ -1,4 +1,4 @@
-import CategoryPage from "@app/_components/CategoryPage";
+import CategoryPage from "@/app/_components/CategoryPage";
 
 export default function Category({ params, searchParams }) {
   const { categorized_news } = params;
